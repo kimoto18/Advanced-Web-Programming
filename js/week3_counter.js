a995c9404394ea3dbe7407064d3c16ca246bf614
Kyle Kimoto <KRK110@pitt.edu>
2024-01-31 17:29:46 -0500

    update

--- js/week3_counter.js
+++ js/week3_counter.js
@@ -1,2 +1,21 @@
-let name = 0
-console.log('0');
+let counterValue = 0;
+
+function updateCounter(){
+    console.log("Counter: " + counterValue);
+}
+
+function increment() {
+    counterValue++;
+    updateCounter();
+}
+
+function decrement() {
+    counterValue--;
+    updateCounter();
+}
+
+function clearCounter() {
+    counterValue = 0;
+    updateCounter();
+}
+
