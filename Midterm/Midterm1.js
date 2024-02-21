7d52fcbf08c099b401b1d8b248ac99a87f4862b7
Kyle Kimoto <KRK110@pitt.edu>
2024-02-20 20:24:46 -0500

    update

--- Midterm/Midterm1.js
+++ Midterm/Midterm1.js
@@ -1,17 +1,17 @@
 // Function of coin toss
-function tossCoin(userChoice) {
+function tossCoin(userMove) {
     var choices = ['heads', 'tails'];
-    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    var computerMove = choices[Math.floor(Math.random() * choices.length)];
 
     var resultElement = document.getElementById('result');
 
-    if (userChoice === computerChoice) {
+    if (userMove === computerMove) {
         // If computer picks same as user
-        resultElement.textContent = "You picked " + userChoice + ", Computer picked " + computerChoice + " – It's a tie!";
-    } else if (userChoice === 'heads' && computerChoice === 'tails') {
+        resultElement.textContent = "You picked " + userMove + ", Computer picked " + computerMove + ". It's a tie!";
+    } else if (userMove === 'heads' && computerMove === 'tails') {
         // If user picks heads and comp picks tails
         resultElement.textContent = "You picked heads, Computer picked tails. You Win!";
-    } else if (userChoice === 'tails' && computerChoice === 'heads') {
+    } else if (userMove === 'tails' && computerMove === 'heads') {
         // If user picks tails and comp picks heads
         resultElement.textContent = "You picked tails, Computer picked heads. You Lose!";
     }
